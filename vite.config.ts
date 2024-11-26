d18888259c7cbd39b00a89b821b25e146b4ab2fd
Sai Pavan <nanisaipavan1234@gmail.com>
2024-11-26 20:13:36 +0530

    third commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     exclude: ['lucide-react'],
+//   },
+// });
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/newrepo/", // Add this line to set the base URL for GitHub Pages
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
 });
